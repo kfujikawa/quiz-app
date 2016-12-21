c1256680f9729e22f0eaac65b2bfb54a684843d7
Kinuyo Fujikawa <kinuyofujikawa@gmail.com>
2016-12-21 14:55:01 -0800

    question is rendering based on question currently tracked via postiion index in question array

--- quiz.js
+++ quiz.js
@@ -12,19 +12,34 @@ var state = {
 // State modification functions
 function getCurrentQuestion(state, index){
 	var currentQuestion = [state.questionNumber[index], state.question[index], state.ans1[index], state.ans2[index], state.ans3[index], state.ans4[index]];
-	console.log(currentQuestion);
 	state.currentQuestion.push(currentQuestion);
+	console.log(currentQuestion);
 }
-
 getCurrentQuestion(state, 0);
 
 // Render functions
-function renderQuestionNum(state, key, value){
-	// document.getElementByID("question").innerHTML = "<li>" + "<h1>" + "Question: " + currentQuestion[0] + "</h1>";
-	document.getElementById("question").innerHTML = "<li>" + "<h1>" + "Question: " + state.currentQuestion[key][value] + "</h1>" + "</li>";
-}
+function renderQuestion(state, index){
+	var questionNum = state.questionNumber[index];
+	var questionText = state.question[index];
+	var answer1 = state.ans1[index];
+	var answer2 = state.ans2[index];
+	var answer3 = state.ans3[index];
+	var answer4 = state.ans4[index];
 
-renderQuestionNum(state, 0, 0);
+	document.getElementById("question").innerHTML = 
+		"<li>" + 
+			"<h1>" + "Question: " + questionNum + "</h1>" + 
+			"<p class='questionText'>" + questionText + "</p>" + 
+			"<form class='submitAnswer'>" + 
+				"<input class='radioButton' type='radio' name='answer' value='ans1'> " + answer1 + "<br>" +
+				"<input class='radioButton' type='radio' name='answer' value='ans2'> " + answer2 + "<br>" +
+				"<input class='radioButton' type='radio' name='answer' value='ans3'> " + answer3 + "<br>" +
+				"<input class='radioButton' type='radio' name='answer' value='ans4'> " + answer4 + "</li>" + "<br>" + 
+				"<input class='button type='submit' name='submit' value='Submit'>" +
+			"</form>" + 
+		"</li>";
+}
+renderQuestion(state, 0);
 
 // Event listeners
 // $('.shopping-list-add').submit(function(event) {
