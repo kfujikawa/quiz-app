d72e2d65f16fb1b41a743b6bf6fdeed84582172d
Kinuyo Fujikawa <kinuyofujikawa@gmail.com>
2016-12-22 13:17:51 -0800

    refactored question structure and got incorrect alert and next functionality for correct answer working

--- quiz.js
+++ quiz.js
@@ -1,49 +1,75 @@
 // Single state object
-var state = {
-    questionNumber: [1,2,3,4,5],
-    question: ["What is Elvis Presley’s middle name?", "What is the lowest male voice? ", "Which famous group was once known as The Quarrymen?", "What is Madonna’s full name?", "What is the noisy invention of Louis Glass in 1890 called?"],
-    ans1: ["James", "Countertenor", "The Rolling Stones", "Madonna", "Jukebox"],
-    ans2: ["Aaron", "Tenor", "Led Zeppelin", "Madonna Ciccone", "Gramophone"],
-    ans3: ["David", "Baritone", "The Beach Boys", "Louise Ciccone", "Hearing aid"],
-    ans4: ["Seth", "Bass", "The Beatles", "Madonna Louise Ciccone", "Sousaphone"],
-    currentQuestion: [], 
-};
+var questions = [
+	{
+		n: 1, 
+		q: "What is Elvis Presley’s middle name?",
+		a: ["James", "Aaron", "David", "Seth"], 
+		c: "Aaron"
+	}, 
+	{
+		n: 2, 
+		q: "What is the lowest male voice?",
+		a: ["Countertenor", "Tenor", "Baritone", "Bass"], 
+		c: "Bass"
+	}, 
+	{
+		n: 3, 
+		q: "Which famous group was once known as The Quarrymen?",
+		a: ["The Rolling Stones", "Led Zeppelin", "The Beach Boys", "The Beatles"], 
+		c: "The Beatles"
+	}, 
+	{
+		n: 4, 
+		q: "What is Madonna’s full name?",
+		a: ["Madonna", "Madonna Ciccone", "Louise Ciccone", "Madonna Louise Ciccone"], 
+		c: "Madonna Louise Ciccone"
+	}, 
+	{
+		n: 5, 
+		q: "What is the noisy invention of Louis Glass in 1890 called?",
+		a: ["Jukebox", "Gramophone", "Hearing aid", "Sousaphone"], 
+		c: "Jukebox"
+	}, 
+];
+
+var currentQuestion = 0;
 
 // State modification functions
-function getCurrentQuestion(state, index){
-	var currentQuestion = [state.questionNumber[index], state.question[index], state.ans1[index], state.ans2[index], state.ans3[index], state.ans4[index]];
-	state.currentQuestion.push(currentQuestion);
-	console.log(currentQuestion);
-}
-getCurrentQuestion(state, 0);
+
 
 // Render functions
-function renderQuestion(state, index){
-	var questionNum = state.questionNumber[index];
-	var questionText = state.question[index];
-	var answer1 = state.ans1[index];
-	var answer2 = state.ans2[index];
-	var answer3 = state.ans3[index];
-	var answer4 = state.ans4[index];
+function renderQuestion(){
+	var question = questions[currentQuestion];
 
-	document.getElementById("question").innerHTML = 
+	$(".question").append(
 		"<li>" + 
-			"<h1>" + "Question: " + questionNum + "</h1>" + 
-			"<p class='questionText'>" + questionText + "</p>" + 
-			"<form class='submitAnswer'>" + 
-				"<input class='radioButton' type='radio' name='answer' value='ans1'> " + answer1 + "<br>" +
-				"<input class='radioButton' type='radio' name='answer' value='ans2'> " + answer2 + "<br>" +
-				"<input class='radioButton' type='radio' name='answer' value='ans3'> " + answer3 + "<br>" +
-				"<input class='radioButton' type='radio' name='answer' value='ans4'> " + answer4 + "</li>" + "<br>" + 
-				"<input class='button type='submit' name='submit' value='Submit'>" +
-			"</form>" + 
-		"</li>";
-}
-renderQuestion(state, 0);
+			"<h1>" + "Question: " + question.n + "</h1>" + 
+			"<p class='questionText'>" + question.q + "</p>" + 
+			"<form class='submitAnswer'>");
+
+	function renderRadioButtons(){
+		for(var i = 0; i < question.a.length; i++){
+			$(".submitAnswer").append("<input type='radio' name='answer' value='" + question.a[i] + "'> " + question.a[i] + "<br>");
+		};
+	};
+
+	renderRadioButtons();
+};
+
+renderQuestion();
+validateQuestion();
 
 // Event listeners
-// $('.shopping-list-add').submit(function(event) {
-//     event.preventDefault();
-//     addItem(state, $('.shopping-list-add-input').val());
-//     renderList(state, $('.shopping-list'));
-// });
+
+function validateQuestion() {
+	$('.submitAnswer input').on('click', function() {
+   		if($("input:radio[name=answer]:checked").val() === questions[currentQuestion].c) {
+			currentQuestion++;
+			$('.question').empty();
+			renderQuestion();
+		}
+		else {
+			alert("Answer is incorrect!  Correct answer is " + questions[currentQuestion].c);
+		}
+	});
+};
